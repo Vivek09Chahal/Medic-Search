5afbab959100cd543513e41d210406ac09501658
Vivek <vivekchahal1156@gmail.com>
2024-11-06 16:36:26 +0530

    completed gameView

---
+++ MedicalDict/TipFiles/TipImageButton.swift
@@ -0,0 +1,18 @@
+//
+//  TipImageButton.swift
+//  MedicalDict
+//
+//  Created by Vivek Chahal on 11/2/24.
+//
+
+import SwiftUI
+
+struct TipImageButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TipImageButton()
+}
